5267f9a49630ce6be2466dfbb5e447f12f70c796
dcdanny945 <147899532+dcdanny945@users.noreply.github.com>
2024-06-26 17:21:51 +1000

    Update toggle on btn

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// alert(`Hello! from the other side`);
+const btn = document.querySelectorAll(`.questions`);
+
+
+btn.forEach((questionAddEventListener)=>{
+
+   questionAddEventListener.addEventListener(`click`,(event)=>{
+
+    const selectedQ = event.target.parentElement.id;
+    const selectedA = document.querySelector(`.answer-${selectedQ}`);
+    console.log(selectedA);
+
+    selectedA.classList.toggle(`open`);
+
+   })
+})
