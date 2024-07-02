a8838399470331ca3094447090d13728767a3141
dcdanny945 <147899532+dcdanny945@users.noreply.github.com>
2024-07-02 14:12:37 +1000

    Update  changing img src

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 const btn = document.querySelectorAll(`.questions`);
 
 
-btn.forEach((questionAddEventListener)=>{
-
-   questionAddEventListener.addEventListener(`click`,(event)=>{
-
-    const selectedQ = event.target.parentElement.id;
-    const selectedA = document.querySelector(`.answer-${selectedQ}`);
-    console.log(selectedA);
-
-    selectedA.classList.toggle(`open`);
-
+btn.forEach((makingBtn)=>{
+   makingBtn.addEventListener(`click`, (e)=>{
+      toggle(e)
    })
 })
+
+
+function toggle(e){
+
+ const clickedElement = e.target;
+ const selectedQ = clickedElement.parentElement.id;
+ const selectedA = document.querySelector(`.answer-${selectedQ}`);
+
+ selectedA.classList.toggle(`open`);
+
+   const nextElement = clickedElement.nextElementSibling;
+   
+   const currentSrc = nextElement.src;   
+
+
+      if(currentSrc.includes(`icon-minus.svg`)){     
+         nextElement.src = `./assets/images/icon-plus.svg`;
+      } else if(currentSrc.includes(`icon-plus.svg`)){  
+         nextElement.src = `./assets/images/icon-minus.svg`;
+      }
+      
+   
+}
+
+
